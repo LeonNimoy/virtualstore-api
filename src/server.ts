a22d02caefa3d9c9a29ad826765f3b1395696539
LeonNimoy <leonardo.msc42@gmail.com>
2020-09-22 21:24:05 -0300

    feat: server file created

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import express, { json } from 'express';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+
+app.use(json());
+
+app.get('/', (req, resp) => {
+  return resp.json({ message: 'Hello World' });
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log('Server Started');
+});
