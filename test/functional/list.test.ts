a7854a5ac20c26a74c607c9f969853e217362e51
LeonNimoy <leonardo.msc42@gmail.com>
2020-09-24 14:25:28 -0300

    feat:[wip] list produtcs test updated

--- test/functional/list.test.ts
+++ test/functional/list.test.ts
@@ -1,31 +1,31 @@
+import { Product } from '@src/models/Product';
+
 describe('Products list functional tests', () => {
+  beforeEach(async () => {
+    await Product.deleteMany({});
+    const defaultProduct = {
+      name: 'Fantasia Homem-Aranha',
+      tag: ['aranha', 'fatasia'],
+      description: 'Um grande fatasia',
+      value: 65.87,
+      quantity: 6,
+    };
+    const product = new Product(defaultProduct);
+    await product.save();
+  });
   it('should return the products with just a few times', async () => {
     const { body, status } = await global.testRequest.get('/list');
     expect(status).toBe(200);
-    expect(body).toEqual([
-      {
-        product: [
-          {
-            name: 'Mesa',
-            tag: ['mesa', 'madeira'],
-            description: 'mesa de jantar para quatro pessoas',
-            value: 225.99,
-            quantity: 46,
-          },
-        ],
-      },
-
-      {
-        product: [
-          {
-            name: 'cadeira',
-            tag: ['festa', 'cadeira'],
-            description: 'cadeira de festa ultra resistente',
-            value: 56.99,
-            quantity: 16,
-          },
-        ],
-      },
-    ]);
+    expect(body).toEqual(
+      expect.objectContaining([
+        {
+          name: 'Fantasia Homem-Aranha',
+          tag: ['aranha', 'fatasia'],
+          description: 'Um grande fatasia',
+          value: 65.87,
+          quantity: 6,
+        },
+      ]),
+    );
   });
 });
