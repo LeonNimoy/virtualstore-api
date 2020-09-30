2b5c1f3f49c115a6f9cc89feb49ebdb5bc6a06c7
LeonNimoy <leonardo.msc42@gmail.com>
2020-09-30 19:27:04 -0300

    feat: upload config file created

---
+++ src/config/upload.ts
@@ -0,0 +1,45 @@
+import multer, { StorageEngine } from 'multer';
+import crypto from 'crypto';
+import path from 'path';
+
+const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp');
+
+interface IUploadConfig {
+  driver: 's3' | 'disk';
+
+  tmpFolder: string;
+  uploadsFolder: string;
+
+  multer: {
+    storage: StorageEngine;
+  };
+
+  config: {
+    disk: {
+      bucket: string;
+    };
+  };
+}
+
+export default {
+  driver: process.env.STORAGE_DRIVER,
+  tmpFolder,
+  uploadsFolder: path.resolve(tmpFolder, 'uploads'),
+
+  multer: {
+    storage: multer.diskStorage({
+      destination: tmpFolder,
+      filename(request, file, callback) {
+        const fileHash = crypto.randomBytes(10).toString('hex');
+        const filename = `${fileHash}-${file.originalname}`;
+        return callback(null, filename);
+      },
+    }),
+  },
+
+  config: {
+    disk: {
+      bucket: 'images-test-nimoy',
+    },
+  },
+} as IUploadConfig;
