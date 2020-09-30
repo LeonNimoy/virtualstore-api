c17232717f12651755c18a7d1d21f9ce425bfeab
LeonNimoy <leonardo.msc42@gmail.com>
2020-09-30 19:29:44 -0300

    feat: S3 class created

---
+++ src/container/implementations/S3StorageProvider.ts
@@ -0,0 +1,55 @@
+import fs from 'fs';
+import path from 'path';
+import mime from 'mime';
+
+import aws, { S3 } from 'aws-sdk';
+import uploadConfig from '../../config/upload';
+
+class DiskStorageProvider {
+  private client: S3;
+
+  constructor() {
+    this.client = new aws.S3({
+      accessKeyId: process.env.AWS_ACCES_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+      region: 'us-east-1',
+    });
+  }
+
+  public async saveFile(file: string): Promise<string> {
+    const originalPath = path.resolve(uploadConfig.tmpFolder, file);
+
+    const fileContent = await fs.promises.readFile(originalPath);
+
+    const ContentType = mime.getType(originalPath);
+
+    if (!ContentType) {
+      throw new Error('File not Found');
+    }
+
+    await this.client
+      .putObject({
+        Bucket: 'images-test-nimoy',
+        Key: file,
+        ACL: 'public-read',
+        Body: fileContent,
+        ContentType,
+      })
+      .promise();
+
+    await fs.promises.unlink(originalPath);
+
+    return file;
+  }
+
+  public async deleteFile(file: string): Promise<void> {
+    await this.client
+      .deleteObject({
+        Bucket: 'images-test-nimoy',
+        Key: file,
+      })
+      .promise();
+  }
+}
+
+export default DiskStorageProvider;
