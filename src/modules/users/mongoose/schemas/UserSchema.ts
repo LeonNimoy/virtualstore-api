fb9455c7edcbec182e1d3e61c04930b105fa8e36
LeonNimoy <leonardo.msc42@gmail.com>
2020-10-07 12:37:03 -0300

    refactor: user schema updated

--- src/modules/users/mongoose/schemas/UserSchema.ts
+++ src/modules/users/mongoose/schemas/UserSchema.ts
@@ -2,33 +2,37 @@ import mongoose, { Document, Model } from 'mongoose';
 
 import IUserEntity from '../../entities/IUserEntity';
 
-const schema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
+const schema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: Number,
+      required: true,
+    },
+    cpf: {
+      type: Number,
+      required: true,
+    },
+    address: {
+      type: Number,
+      required: true,
+    },
   },
-  email: {
-    type: String,
-    unique: true,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  phone: {
-    type: Number,
-    required: true,
-  },
-  cpf: {
-    type: Number,
-    required: true,
-  },
-  address: {
-    type: Number,
-    required: true,
-  },
-});
+
+  { timestamps: { createdAt: 'created_at' } },
+);
 
 interface IUserModel extends IUserEntity, Document {}
 export const UserSchema: Model<IUserModel> = mongoose.model('User', schema);
