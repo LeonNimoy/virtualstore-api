31dabaabb158b92992b80427c001073d27273396
LeonNimoy <leonardo.msc42@gmail.com>
2020-10-07 15:44:16 -0300

    refactor:typo id fixed

--- src/databases/mongoose/schemas/ProductSchema.ts
+++ src/databases/mongoose/schemas/ProductSchema.ts
@@ -1,33 +1,46 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-underscore-dangle */
 import mongoose, { Document, Model } from 'mongoose';
 
 import IProductEntity from '../../../entities/IProductEntity';
 
-const schema = new mongoose.Schema({
-  name: {
-    type: String,
-    unique: true,
-    required: true,
+const schema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    tags: {
+      type: [String],
+    },
+    description: {
+      type: String,
+    },
+    image: {
+      type: String,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    quantity: {
+      type: Number,
+      required: true,
+    },
   },
-  tags: {
-    type: [String],
+  {
+    toJSON: {
+      transform: (_, ret): void => {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.__v;
+      },
+    },
   },
-  description: {
-    type: String,
-  },
-  image: {
-    type: String,
-  },
-  price: {
-    type: Number,
-    required: true,
-  },
-  quantity: {
-    type: Number,
-    required: true,
-  },
-});
+);
 
-interface IProductModel extends IProductEntity, Document {}
+interface IProductModel extends Omit<IProductEntity, '_id'>, Document {}
 export const ProductSchema: Model<IProductModel> = mongoose.model(
   'Product',
   schema,
