b8a8ebb1dd5b6bab8899e6658b73498731facbae
LeonNimoy <leonardo.msc42@gmail.com>
2020-10-14 09:41:14 -0300

    refactor: product schema updated

--- src/modules/users/databases/mongoose/schemas/UserSchema.ts
+++ src/modules/users/databases/mongoose/schemas/UserSchema.ts
@@ -1,5 +1,3 @@
-/* eslint-disable no-param-reassign */
-/* eslint-disable no-underscore-dangle */
 import mongoose, { Document, Model } from 'mongoose';
 
 import IUserEntity from '../../../entities/IUserEntity';
@@ -45,5 +43,5 @@ const schema = new mongoose.Schema(
   },
 );
 
-interface IUserModel extends Omit<IUserEntity, '_id'>, Document {}
+interface IUserModel extends Omit<IUserEntity, 'id'>, Document {}
 export const UserSchema: Model<IUserModel> = mongoose.model('User', schema);
