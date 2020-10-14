0fa26fce8dec2849aba1ee040a02f65297d11678
LeonNimoy <leonardo.msc42@gmail.com>
2020-10-14 17:21:26 -0300

    refactor: errors standardized for productImage controller

--- src/modules/products/controllers/ProductImageController.ts
+++ src/modules/products/controllers/ProductImageController.ts
@@ -1,22 +1,14 @@
 import { Request, Response } from 'express';
-import mongoose from 'mongoose';
 import { container } from 'tsyringe';
 
 import UpdateProductImageService from '../services/UpdateProductImageService';
 
 export default class ProductImageController {
   public async update(req: Request, res: Response): Promise<Response> {
-    try {
-      const updateProductImage = container.resolve(UpdateProductImageService);
-      const productImage = await updateProductImage.execute({
-        imageFilename: req.file.filename,
-      });
-      return res.status(201).json(productImage);
-    } catch (err) {
-      if (err instanceof mongoose.Error.ValidationError) {
-        return res.status(422).json({ err: err.message });
-      }
-      return res.status(500).json({ err: 'Internal Server Error' });
-    }
+    const updateProductImage = container.resolve(UpdateProductImageService);
+    const productImage = await updateProductImage.execute({
+      imageFilename: req.file.filename,
+    });
+    return res.status(201).json(productImage);
   }
 }
