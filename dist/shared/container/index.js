ff8791feb126ca5666a32206d93e59db3b610f09
LeonNimoy <leonardo.msc42@gmail.com>
2020-10-17 11:50:59 -0300

    feat:dist

--- dist/shared/container/index.js
+++ dist/shared/container/index.js
@@ -5,7 +5,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var tsyringe_1 = require("tsyringe");
 require("../../modules/users/providers");
+var S3StorageProvider_1 = __importDefault(require("./implementations/S3StorageProvider"));
 var ProductsRepository_1 = __importDefault(require("../../modules/products/repositories/ProductsRepository"));
 var UsersRepository_1 = __importDefault(require("../../modules/users/repositories/UsersRepository"));
 tsyringe_1.container.registerSingleton('ProductsRepository', ProductsRepository_1.default);
 tsyringe_1.container.registerSingleton('UsersRepository', UsersRepository_1.default);
+tsyringe_1.container.registerSingleton('S3StorageProvider', S3StorageProvider_1.default);
