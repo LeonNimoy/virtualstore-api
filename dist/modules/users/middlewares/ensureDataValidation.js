f403217613849c03994a305f18846230410daf53
LeonNimoy <leonardo.msc42@gmail.com>
2020-10-29 19:05:17 -0300

    refactor:dist

--- dist/modules/users/middlewares/ensureDataValidation.js
+++ dist/modules/users/middlewares/ensureDataValidation.js
@@ -6,18 +6,18 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var email_validator_1 = __importDefault(require("email-validator"));
 var AppError_1 = __importDefault(require("../../../shared/errors/AppError"));
 function ensureDataValidation(req, res, next) {
-    var _a = req.body, email = _a.email, password = _a.password;
-    if (!email || !password) {
-        throw new AppError_1.default('Email or Password  is missing!', 401);
+    var _a = req.body, name = _a.name, email = _a.email, password = _a.password;
+    if (!email || !password || !name) {
+        throw new AppError_1.default('Nome, Email e Senha devem ser informados ', 401);
     }
     var checkEmailFormat = email_validator_1.default.validate(email);
     if (!checkEmailFormat) {
-        throw new AppError_1.default('Email format invalid!');
+        throw new AppError_1.default('Email inválido!');
     }
     var passwordValidation = new RegExp(/^.{6,}$/);
     var checkPasswordFormat = passwordValidation.test(password);
     if (!checkPasswordFormat) {
-        throw new AppError_1.default('The password must have a minimum of 6 characters!');
+        throw new AppError_1.default('A senha deve ter no mínimo de 6 caracteres!');
     }
     return next();
 }
